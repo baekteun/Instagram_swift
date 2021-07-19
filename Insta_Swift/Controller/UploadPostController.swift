3f3fc635d8de3a2ab859d60c94081df3b7422ba4
baegteun <baegteun@gmail.com>
2021-07-19 13:07:24 +0900

    uploadController

---
+++ Insta_Swift/Controller/UploadPostController.swift
@@ -0,0 +1,20 @@
+//
+//  UploadPostController.swift
+//  Insta_Swift
+//
+//  Created by baegteun on 2021/07/19.
+//
+
+import UIKit
+
+class UploadPostController: UIViewController{
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: -Helpers
+    
+}
