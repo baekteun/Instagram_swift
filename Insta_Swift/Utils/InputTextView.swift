3d7feece53d6bcf40cb920b82b865ce3d58ae2aa
baegteun <baegteun@gmail.com>
2021-07-19 13:55:46 +0900

    if textdidchange,  hidden placeholder

--- Insta_Swift/Utils/InputTextView.swift
+++ Insta_Swift/Utils/InputTextView.swift
@@ -8,5 +8,34 @@
 import UIKit
 
 class InputTextView: UITextView {
+    // MARK: - Properties
+    var placeholderText: String?{
+        didSet{ placeholderLabel.text = placeholderText }
+    }
+    private let placeholderLabel: UILabel = {
+        let lb = UILabel()
+        lb.textColor = .lightGray
+        
+        return lb
+    }()
     
+    // MARK: - Lifecycle
+    override init(frame: CGRect, textContainer: NSTextContainer?){
+        super.init(frame: frame, textContainer: textContainer)
+        
+        addSubview(placeholderLabel)
+        placeholderLabel.anchor(top: topAnchor,left: leftAnchor, paddingTop: 6, paddingLeft: 8)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleTextDidChange), name: UITextView.textDidChangeNotification, object: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Actions
+    
+    @objc func handleTextDidChange(){
+        placeholderLabel.isHidden = !text.isEmpty
+    }
 }
